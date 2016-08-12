db2383c95bc2385c1690d843030f800593d4dc36
Damien Whitten <damien@rebase.com.au>
2016-08-12 16:33:26 -0400

    init

---
+++ envconf.go
@@ -0,0 +1,27 @@
+package envconf
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+func EnvStruct(dest interface{}) error {
+	rt := reflect.TypeOf(dest).Elem()
+	rv := reflect.ValueOf(dest).Elem()
+	for i := 0; i < rv.NumField(); i++ {
+		tag := rt.Field(i).Tag
+		envName := tag.Get("env")
+		envVal := os.Getenv(envName)
+		if envVal == "" {
+			defaultValue := tag.Get("default")
+			if defaultValue == "" {
+
+				return fmt.Errorf("Required ENV var not set: %v", tag)
+			}
+			envVal = defaultValue
+		}
+		rv.Field(i).SetString(envVal)
+	}
+	return nil
+}
